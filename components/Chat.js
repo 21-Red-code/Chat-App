754ee85e0f569a6ca293018dc08d2891824547d4
21-Red-code <shermang37@gmail.com>
2022-02-09 13:17:57 +0100

    installing and importing GiftedChat and Bubble
    when this commited:
    it adds componentDidMount, constructor, onSend and renderBubble methods

--- components/Chat.js
+++ components/Chat.js
@@ -1,23 +1,79 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Platform, KeyboardAvoidingView } from 'react-native';
+import { GiftedChat, Bubble } from 'react-native-gifted-chat';
+
+const firebase = require('firebase');
+require('firebase/firestore');
 
 export default class Chat extends React.Component {
-  render() {
-    //entered name state from Start screen gets displayed in status bar at the top of the app
-    let name = this.props.route.params.name;
-    this.props.navigation.setOptions({ title: name});
 
-    const { bgColor } = this.props.route.params;
+  constructor() {
+    super();
+    this.state = {
+      message: [],
+    };
+  }
+
+  componentDidMount() {
+    // feeding name prop
+    let name = this.props.route.params.name;
+
+    // setting default messages
+    this.setState({
+      messages: [
+        
+        {
+          _id: 1,
+          text: 'Hello developer!',
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'React Native',
+            avatar: 'https://placeimg.com/140/140/any',
+          }
+        },
+        {
+          _id: 2,
+          text: `${name} has entered the chat`,
+          createdAt: new Date(),
+          system: true,
+        },
+      ]
+    })
+  }
+  // when message sends, appends to message array
+  onSend(messages = []) {
+    this.setState((previousState) => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }));
+  }
+  renderBubble(props) {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          right: {
+            backgroundColor: '#1b72ba'
+          }
+        }}
+      />
+    )
+  }  
+
+  render() {
+    // feeding bgColor state
+    let { bgColor } = this.props.route.params;
 
     return (
-      <View style={{
-        flex: 1, 
-        justifyContent: 'center', 
-        alignItems: 'center',
-        backgroundColor: bgColor
-        }}>
-        <Text>welcome</Text>
+      <View style={{backgroundColor: bgColor, flex: 1}}>      
+      <GiftedChat
+        renderBubble={this.renderBubble.bind(this)}
+        messages={this.state.messages}
+        onSend={(messages) => this.onSend(messages)}
+        user={{_id: 1}}
+      />
+      { Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null }
       </View>
-    )
+    );
   }
 }
